4916e96872a0e0950cfc4e69f563a287214bb0a7
PetrenkoMS <el-2002@mail.ru>
2022-04-14 15:19:28 +0300

    ALL (with hash) but Without cleaning

--- src/main/webapp/js/registration.js
+++ src/main/webapp/js/registration.js
@@ -1,6 +1,13 @@
 function goRegistr() {
     var nl = document.getElementById("newLogin").value;
-    var np = document.getElementById("newPassword").value;
+    function hashCode(s) {
+        for(var i = 0, h = 0; i < s.length; i++)
+            h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+        return h;
+    }
+
+    var np_w = document.getElementById("newPassword").value;
+    var np = hashCode(np_w);
     var nlp = nl + "," + np;
 
     $.ajax({
--- src/main/webapp/js/vxod.js
+++ src/main/webapp/js/vxod.js
@@ -1,34 +1,40 @@
 function vhodIn() {
     var login = $('#login').val();
-    var password = $('#password').val();
-    temp=login +"," + password;
+    function hashCode(s) {
+        for(var i = 0, h = 0; i < s.length; i++)
+            h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+        return h;
+    }
+    var password_w = $('#password').val();
+    var password = hashCode(password_w);
+    var temp=login +"," + password;
     $.ajax({
         type: "POST",
         url: "/inLog",
         data: temp,
         success: function(result){
             alert('Аутентификация прошла успешно! \n Привет ' + login +"!");
-                document.getElementById("login").value="";
-                document.getElementById("password").value="";
+            document.getElementById("login").value="";
+            document.getElementById("password").value="";
 
-                function getCookie(name) {
-                    let match = document.cookie.split('; ').find(row => row.startsWith(`${name}=`));
-                    return match ? match.split('=')[1] : undefined;
+            function getCookie(name) {
+                let match = document.cookie.split('; ').find(row => row.startsWith(`${name}=`));
+                return match ? match.split('=')[1] : undefined;
+            }
+
+            var cookies= document.cookie;
+            cookieValue = getCookie("sait");
+            console.log("cookiesVal: " + cookieValue);
+            console.log(typeof cookieValue);
+            if (cookieValue) {
+                if (cookieValue == "main") {
+                    window.location.href="/";
+                }
+                else {
+                    window.location.href="/"+cookieValue;
                 }
 
-                var cookies= document.cookie;
-                cookieValue = getCookie("sait");
-                console.log("cookiesVal: " + cookieValue);
-                console.log(typeof cookieValue);
-                if (cookieValue) {
-                    if (cookieValue == "main") {
-                        window.location.href="/";
-                    }
-                    else {
-                        window.location.href="/"+cookieValue;
-                    }
-
-                }
+            }
 //                window.location.reload();
         },
         error: function(result){
